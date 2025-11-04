3a5ccab53d4cd1b436a8cd7ad700a35e507b01e9
type <type>
2025-11-03 23:11:32 -0800

    Made more updates to build script

--- build.sh
+++ build.sh
@@ -22,25 +22,31 @@ pip install -r requirements.txt
 # Then add that to mkdocs
 
 # Cloning the TheAtlasEngine repository
-git clone https://github.com/engine3d-dev/TheAtlasEngine
-
 # We check if TheAtlasEngine directory exists then we remove it and re-clone repository again
 if ! [ -d TheAtlasEngine ]; then
+    echo "Cloning TheAtlasEngine Repo..."
     git clone https://github.com/engine3d-dev/TheAtlasEngine
 else
+    echo "TheAtlasEngine Repo Found!"
     rm -rf TheAtlasEngine
+    echo "Deleted TheAtlasEngine/ Repo"
+    echo "Cloning TheAtlasEngine Repo..."
     git clone https://github.com/engine3d-dev/TheAtlasEngine
 fi
 
 # Copies Doxyfile to TheAtlasEngine directory to modify it for the documentation website
+echo "cp Doxyfile TheAtlasEngine/Doxyfile"
 cp Doxyfile TheAtlasEngine/Doxyfile
+echo "ls TheAtlasEngine | grep \"Doxyfile\""
+echo "Output:"
+ls TheAtlasEngine | grep "Doxyfile"
 
 # Generating the doxygen documentation from the specified Doxyfile in the repository
 doxygen TheAtlasEngine/Doxyfile
 
-# Copying the doxygen documentation to the mkdocs/html directory
+# # Copying the doxygen documentation to the mkdocs/html directory
 cp -R docs/html mkdocs/html
 
 
-chmod +x apply_css.sh
+# chmod +x apply_css.sh
 ./apply_css.sh
